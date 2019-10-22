e629c78017cded8b13881706c95526eb7c2b6172
Julia Aasmaa <my_e_10@hotmail.com>
2019-10-22 15:51:58 +0300

    Add temp Converter

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 window.addEventListener('load', ()=> {
-    //let long = 24.7037952;
+   //let long = 24.7037952;
    // let lat = 59.4075648;
    let long;
    let lat;
+
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let locationTimeZone = document.querySelector(".location-timezone");
+    let temperatureSection = document.querySelector(".degree-section");
+    let temperatureSectionSpan = document.querySelector(".degree-section span");
+
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -15,9 +22,31 @@ window.addEventListener('load', ()=> {
 
             fetch(api)
             .then(response => {
-                console.log(response.json());
+                console.log(response);
+                return response.json();
+            })
+            .then(data => {
+                const {temperature, summary, icon} = data.currently;
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimeZone.textContent = data.timezone;
+
+                let celsius = (temperature - 32) * (5/9);
+
+                temperatureSection.addEventListener('click', () => {
+                    if(temperatureSectionSpan.textContent === "F"){
+                        temperatureSectionSpan.textContent = "℃";
+                        temperatureDegree.textContent = Math.floor(celsius);
+                    } else {
+                        temperatureSectionSpan.textContent = "F";
+                        temperatureDegree.textContent = temperature;
+                    }
+                });
+                
             });
-        })
+        });
+
+
         
         
     }
